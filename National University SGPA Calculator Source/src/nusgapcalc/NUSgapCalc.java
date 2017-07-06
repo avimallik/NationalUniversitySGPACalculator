81ca595568d1b09bd2d5302d18d810bf98b4dd49
arm avi <armavicse@gmail.com>
2017-07-06 14:08:20 +0600

    National University Bangladesh SGPA Counter
    National University Bangladesh SGPA Calculator is a SGPA Counter That Calculates Semester Grade Point Averege

---
+++ National University SGPA Calculator Source/src/nusgapcalc/NUSgapCalc.java
@@ -0,0 +1,17 @@
+
+package nusgapcalc;
+
+/**
+ *
+ * @author Arm_AVI
+ */
+public class NUSgapCalc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
